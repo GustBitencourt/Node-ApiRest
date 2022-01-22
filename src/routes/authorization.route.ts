c3e09ccd103790713b870ceda4b597014340795c
GustBitencourt <gubiten@gmail.com>
2022-01-22 14:30:35 -0300

    biblioteca jsonwebtoken

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction} from 'express';
 import ForbiddenError from '../models/errors/forbiddenError.model';
 import userRepository from '../repositories/user.repository';
+import JWT from 'jsonwebtoken';
 
 const authorizationRoute = Router();
 
@@ -32,9 +33,13 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next:NextF
         //verificando usuario e senha
         const user = await userRepository.findByUserAndPassword(username, password);
 
-
-
-
+        //'iss' é o dominio da aplicação geradora de token
+        //'sub' é o assunto do token, mas é utilizado para guardar o id do usuario
+        //'aud' define quem pode utilizar o token
+        //'exp1 data para expiracao do token
+        //'nbf' define data para qual o token não pode ser aceito antes dela
+        //'iat' data de criação do token 
+        //'jti' o id do token
 
         
     } catch (error) {
